09bd6cff0ce4ea78d7c20e69f3d7c6e98cf477db
OliSparrow <jay_jakobsen@outlook.dk>
2024-04-03 15:40:50 +0200

    Pages after Login can switch between one another

--- src/XML/Gui/Controllers/EventPageController.java
+++ src/XML/Gui/Controllers/EventPageController.java
@@ -1,4 +1,61 @@
 package XML.Gui.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class EventPageController {
+    @FXML
+    private Pane accountPane;
+
+
+
+    // FXML Methods
+    @FXML
+    private void handleAccountButtonAction(ActionEvent actionEvent) {
+        accountPane.setVisible(!accountPane.isVisible());
+    }
+
+    @FXML
+    private void goToDashboard(ActionEvent event) {
+        switchScene("/MainPageAdmin.fxml", "EASV Bar");
+    }
+
+    @FXML
+    private void logOut(ActionEvent event) {
+        switchScene("/LoginPage.fxml", "EASV Bar");
+    }
+
+
+
+    // Other Methods
+
+
+    private void switchScene(String fxmlPath, String title) {
+        Stage stage = (Stage) accountPane.getScene().getWindow();
+
+        try {
+            // Load the new scene from the specified FXML path
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            scene.setFill(javafx.scene.paint.Color.valueOf("#131414"));
+
+            stage.setScene(scene);
+            stage.setTitle(title); // Set the stage title
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Could not load the page.");
+            alert.showAndWait();
+        }
+    }
 }
--- src/XML/Gui/Controllers/UserManagementController.java
+++ src/XML/Gui/Controllers/UserManagementController.java
@@ -1,4 +1,84 @@
 package XML.Gui.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class UserManagementController {
+    @FXML
+    private Pane accountPane;
+
+
+
+    // FXML Methods
+    @FXML
+    private void handleAccountButtonAction(ActionEvent actionEvent) {
+        accountPane.setVisible(!accountPane.isVisible());
+    }
+
+    @FXML
+    private void goToDashboard(ActionEvent event) {
+        switchScene("/MainPageAdmin.fxml", "EASV Bar");
+    }
+
+    @FXML
+    private void logOut(ActionEvent event) {
+        switchScene("/LoginPage.fxml", "EASV Bar");
+    }
+
+    @FXML
+    private void goToTickets(ActionEvent event) {
+        // switchScene("/ticketsManagement.fxml", "EASV Bar");
+    }
+
+    @FXML
+    private void handleDeleteCoordinator(ActionEvent event){
+
+    }
+
+    @FXML
+    private void handleDeleteUser(ActionEvent event){
+
+    }
+
+    @FXML
+    private void handleAssign(ActionEvent event){
+
+    }
+
+    @FXML
+    private void handleUnassign(ActionEvent event){
+
+    }
+
+    // Other Methods
+
+
+    private void switchScene(String fxmlPath, String title) {
+        Stage stage = (Stage) accountPane.getScene().getWindow();
+
+        try {
+            // Load the new scene from the specified FXML path
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            scene.setFill(javafx.scene.paint.Color.valueOf("#131414"));
+
+            stage.setScene(scene);
+            stage.setTitle(title); // Set the stage title
+            stage.centerOnScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Could not load the page.");
+            alert.showAndWait();
+        }
+    }
 }
